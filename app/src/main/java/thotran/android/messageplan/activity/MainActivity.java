526f53ad71e1523f941aff437e340d8f46e4dbc3
Tho Tran <trandev90@gmail.com>
2017-11-29 02:56:19 +0700

    implement ListView and Custom Adapter

--- app/src/main/java/thotran/android/messageplan/activity/MainActivity.java
+++ app/src/main/java/thotran/android/messageplan/activity/MainActivity.java
@@ -1,31 +1,43 @@
 package thotran.android.messageplan.activity;
 
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 
+import java.util.List;
+
+import thotran.android.messageplan.adapter.MessageAdapter;
 import thotran.android.messageplan.database.AppDatabase;
+import thotran.android.messageplan.entities.Message;
 import thotran.android.messageplan.utils.DatabaseInitializer;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = MainActivity.class.getName();
-    private Button btnClickMe;
+    ListView mListView;
+    List<Message> Messages;
+    MessageAdapter messageAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        btnClickMe = (Button)findViewById(R.id.click_here_btn);
-        btnClickMe.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Log.i("test","Button click");
-                DatabaseInitializer.populateAsync(AppDatabase.getAppDatabase(getBaseContext()));
-            }
-        });
+        initComponent();
+    }
+
+    @SuppressLint("WrongViewCast")
+    private void initComponent(){
+        mListView = (ListView)findViewById(R.id.ivListMessage);
+        Messages = AppDatabase.getAppDatabase(getBaseContext()).messageDao().getAll();
+        messageAdapter = new MessageAdapter(getBaseContext(),R.layout.item_message,Messages);
+        mListView.setAdapter(messageAdapter);
     }
 
     @Override
---
+++ app/src/main/java/thotran/android/messageplan/adapter/MessageAdapter.java
@@ -0,0 +1,63 @@
+package thotran.android.messageplan.adapter;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.util.List;
+
+import thotran.android.messageplan.activity.R;
+import thotran.android.messageplan.entities.Message;
+
+/**
+ * Created by thotran on 11/29/17.
+ */
+
+public class MessageAdapter extends ArrayAdapter {
+    public MessageAdapter(@NonNull Context context, int resource) {
+        super(context, resource);
+    }
+
+    public MessageAdapter(Context context, int resource, List<Message> items) {
+        super(context, resource, items);
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        View v = convertView;
+
+        if (v == null) {
+            LayoutInflater vi;
+            vi = LayoutInflater.from(getContext());
+            v = vi.inflate(R.layout.item_message, null);
+        }
+
+        Message p = (Message) getItem(position);
+
+        if (p != null) {
+            TextView tt1 = (TextView) v.findViewById(R.id.txtTitle);
+            TextView tt2 = (TextView) v.findViewById(R.id.txtReceiver);
+            TextView tt3 = (TextView) v.findViewById(R.id.txtContent);
+
+            if (tt1 != null) {
+                tt1.setText(p.getTitle());
+            }
+
+            if (tt2 != null) {
+                tt2.setText(p.getTo());
+            }
+
+            if (tt3 != null) {
+                tt3.setText(p.getBody());
+            }
+        }
+
+        return v;
+    }
+}
