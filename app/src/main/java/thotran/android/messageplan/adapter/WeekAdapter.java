63f864740f7d706212cbd387154a0ac4dcfc7718
Tho Tran <trandev90@gmail.com>
2017-12-08 00:49:26 +0700

    add missed item

---
+++ app/src/main/java/thotran/android/messageplan/adapter/WeekAdapter.java
@@ -0,0 +1,55 @@
+package thotran.android.messageplan.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.CheckBox;
+
+import thotran.android.messageplan.activity.R;
+
+/**
+ * Created by thotran on 12/7/17.
+ */
+
+public class WeekAdapter extends BaseAdapter {
+
+    Context context;
+    String[] mDays;
+
+    public WeekAdapter(Context context, String[] mDays) {
+        this.context = context;
+        this.mDays = mDays;
+    }
+
+    @Override
+    public int getCount() {
+        return mDays.length;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return mDays[position];
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        View v= convertView;
+        if(v==null){
+            v = LayoutInflater.from(context).inflate(R.layout.item_day,parent,false);
+        }
+
+        CheckBox chkDay;
+        chkDay = (CheckBox)v.findViewById(R.id.chkDay);
+        //chkDay.setId(position);
+        chkDay.setText(getItem(position).toString());
+
+        return v;
+    }
+}
